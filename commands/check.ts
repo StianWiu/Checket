2ac7a2f21c4945c670ab6dc38732a0ed31b075a1
Pignuuu <Pignuuu@gmail.com>
2021-09-25 14:33:18 +0200

    Add feature that allows user to specify how long to load

--- commands/check.ts
+++ commands/check.ts
@@ -2,74 +2,119 @@ import { MessageEmbed } from "discord.js";
 import { ICommand } from "wokcommands";
 
 export default {
-    category: 'Link',
-    description: 'Get screenshot of specified link.',
-    slash: false,
-    guildOnly: true,
+  category: "Link",
+  description: "Get screenshot of specified link.",
+  slash: false,
+  guildOnly: true,
 
-
-    callback: async ({ args, message }) => {
-        // Check if input fits required format.
-        const link = (args[0])
-        if (link.startsWith("http://", 0) || link.startsWith("https://", 0)) {
-            // Create embed
-            const loadingEmbed = new MessageEmbed()
-            .setTitle("Please wait while I check your link.")
-            .setDescription("Please allow up to a minute for this to load.")
-            .setColor('#F86154')
-            .setFooter("Checket", "https://cdn.discordapp.com/avatars/888736693128151103/1cfc286bdcede1eb4d227d53fc5413fb.webp?size=128")
-            // Send embed
-            const waitEmbed = await message.reply({
-                embeds: [loadingEmbed],
-            })
-            // Generate random string to be used for filename of screenshot
-            var randomstring = require("randomstring");
-            const filename = randomstring.generate();
-            // Use puppeteer to grab screenshot of specified url
-            const puppeteer = require("puppeteer");
-            const capture = async () => {
-                const browser = await puppeteer.launch({ defaultViewport: { width: 1920, height: 1080 }, headless: true, args: ['--no-sandbox'] });
-                const page = await browser.newPage();
-                await page.goto(link);
-                await new Promise(resolve => setTimeout(resolve, 5000))
-                await page.screenshot({ path: "./temp/" + filename + ".png" });
-                await browser.close();
-            };
-            capture().then(async (response) => {
-                // Create embed
-                const embed = new MessageEmbed()
-                .setTitle(link)
-                .setColor('#F86154')
-                .setFooter("Checket", "https://cdn.discordapp.com/avatars/888736693128151103/1cfc286bdcede1eb4d227d53fc5413fb.webp?size=128")
-                .setImage("attachment://" + filename + ".png")
-                // Send embed, with file attached
-                const newMessage = await message.reply({
-                    embeds: [embed], files: ['./temp/' + filename + '.png']
-                })
-                waitEmbed.delete()
-                const fs = require('fs')
-                fs.unlinkSync('./temp/' + filename + ".png")
-                return
-            })
-
-        } else {
-            // If input doesn't fit required format
-            const embed = new MessageEmbed()
-                .setTitle("Error")
-                .setDescription("You did not provide a valid link.")
-                .setFooter("Valid link should start with http:// or https://", "https://cdn.discordapp.com/avatars/888736693128151103/1cfc286bdcede1eb4d227d53fc5413fb.webp?size=128")
-                .setColor("RED")
-            if (message === undefined) {
-                return embed
-            } else {
-                const newMessage = await message.reply({
-                    embeds: [embed]
-                })
-                // Delete embed after 5 seconds
-                await new Promise(resolve => setTimeout(resolve, 5 * 1000))
-                newMessage.delete()
-            }
-
-        }
+  callback: async ({ args, message }) => {
+    if (args[0]) {
+      // Check if input fits required format.
+      const link = args[0];
+      if (link.startsWith("http://", 0) || link.startsWith("https://", 0)) {
+        // Create embed
+        const loadingEmbed = new MessageEmbed()
+          .setTitle("Please wait while I check your link.")
+          .setDescription("Please allow up to a minute for this to load.")
+          .setColor("#F86154")
+          .setFooter(
+            "Checket",
+            "https://cdn.discordapp.com/avatars/888736693128151103/1cfc286bdcede1eb4d227d53fc5413fb.webp?size=128"
+          );
+        // Send embed
+        const waitEmbed = await message.reply({
+          embeds: [loadingEmbed],
+        });
+        // Generate random string to be used for filename of screenshot
+        var waited: any = undefined;
+        var randomstring = require("randomstring");
+        const filename = randomstring.generate();
+        // Use puppeteer to grab screenshot of specified url
+        const puppeteer = require("puppeteer");
+        const capture = async () => {
+          const browser = await puppeteer.launch({
+            defaultViewport: { width: 1920, height: 1080 },
+            headless: true,
+            args: ["--no-sandbox"],
+          });
+          const page = await browser.newPage();
+          await page.goto(link);
+          // Check if user specified how long to load website for
+          function isNumeric(value: any) {
+            // Function to check if argument is string or number
+            return /^-?\d+$/.test(value);
+          }
+          if (args[1] && isNumeric(args[1])) {
+            const seconds: any = args[1];
+            waited = true;
+            await new Promise((resolve) =>
+              setTimeout(resolve, parseInt(seconds) * 1000)
+            );
+          } else {
+            waited = false;
+            await new Promise((resolve) => setTimeout(resolve, 5000));
+          }
+          await page.screenshot({ path: "./temp/" + filename + ".png" });
+          await browser.close();
+        };
+        capture().then(async (response) => {
+          // Create embed
+          const embed = new MessageEmbed()
+            .setTitle(link)
+            .setColor("#F86154")
+            .setImage("attachment://" + filename + ".png");
+          if (waited) {
+            embed.setFooter(
+              "Checket, loaded for " + args[1] + " second(s).",
+              "https://cdn.discordapp.com/avatars/888736693128151103/1cfc286bdcede1eb4d227d53fc5413fb.webp?size=128"
+            );
+          } else {
+            embed.setFooter(
+              "Checket",
+              "https://cdn.discordapp.com/avatars/888736693128151103/1cfc286bdcede1eb4d227d53fc5413fb.webp?size=128"
+            );
+          }
+          // Send embed, with file attached
+          const newMessage = await message.reply({
+            embeds: [embed],
+            files: ["./temp/" + filename + ".png"],
+          });
+          waitEmbed.delete();
+          const fs = require("fs");
+          fs.unlinkSync("./temp/" + filename + ".png");
+          return;
+        });
+      } else {
+        // If input doesn't fit required format
+        const embed = new MessageEmbed()
+          .setTitle("Error")
+          .setDescription("You did not provide a valid link.")
+          .setFooter(
+            "A valid link should start with http:// or https://",
+            "https://cdn.discordapp.com/avatars/888736693128151103/1cfc286bdcede1eb4d227d53fc5413fb.webp?size=128"
+          )
+          .setColor("RED");
+        const newMessage = await message.reply({
+          embeds: [embed],
+        });
+        // Delete embed after 5 seconds
+        await new Promise((resolve) => setTimeout(resolve, 5 * 1000));
+        newMessage.delete();
+      }
+    } else {
+      const embed = new MessageEmbed()
+        .setTitle("!Check")
+        .setDescription(
+          "To use the !Check command you need to do `!Check {$URL} {$SECONDS}` the seconds parameter is optional. It chooses how long you want to let the website load. Default is 5 seconds.\n\nSo your command should look like this `!Check https://stianwiu.me 10`"
+        )
+        .setFooter(
+          "A valid link should start with http:// or https://",
+          "https://cdn.discordapp.com/avatars/888736693128151103/1cfc286bdcede1eb4d227d53fc5413fb.webp?size=128"
+        )
+        .setColor("RED");
+      const newMessage = await message.reply({
+        embeds: [embed],
+      });
     }
-} as ICommand
+  },
+} as ICommand;
--- index.ts
+++ index.ts
@@ -1,101 +1,38 @@
-import DiscordJS, { Intents } from 'discord.js'
-import dotenv from 'dotenv'
-import WOKCommands from 'wokcommands'
-import path from 'path'
-dotenv.config()
+import DiscordJS, { Intents } from "discord.js";
+import dotenv from "dotenv";
+import WOKCommands from "wokcommands";
+import path from "path";
+dotenv.config();
 
 const client = new DiscordJS.Client({
-    intents: [ //Specify what intents bot requires.
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-    ]
-})
+  intents: [
+    //Specify what intents bot requires.
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+  ],
+});
 
-client.on('ready', () => {
-    console.log('Bot has started')
+client.on("ready", () => {
+  console.log("Bot has started");
 
-// Settings for WOKCommands.
+  // Settings for WOKCommands.
 
-    new WOKCommands(client, {
-        commandsDir: path.join(__dirname, 'commands'),
-        typeScript: false, //This should be false when deploying to server. Only true when not using compiler.
-        testServers: ['715215005812785182'], //Chooses what server to directly deploy /commands to.
-        disabledDefaultCommands: [ //Disable commands that come pre made with WOKCommands.
-            'channelonly',
-            'command',
-            'language',
-            'prefix',
-            'requiredrole',
-            'slash',
-            'help',
-        ]
-    })
-    .setDefaultPrefix('!')
-})
+  new WOKCommands(client, {
+    commandsDir: path.join(__dirname, "commands"),
+    typeScript: false, //This should be false when deploying to server. Only true when not using compiler.
+    testServers: ["715215005812785182"], //Chooses what server to directly deploy /commands to.
+    disabledDefaultCommands: [
+      //Disable commands that come pre made with WOKCommands.
+      "channelonly",
+      "command",
+      "language",
+      "prefix",
+      "requiredrole",
+      "slash",
+      "help",
+    ],
+  }).setDefaultPrefix("!");
+});
 
-client.login(process.env.TOKEN)
-
-
-
-// const guildId = '715215005812785182'
-// const guild = client.guilds.cache.get(guildId)
-// let commands
-// if (guild) {
-//     commands = guild.commands
-// } else {
-//     commands = client.application?.commands
-// }
-// commands?.create({
-//     name: 'ping',
-//     description: 'Replies with pong',
-// })
-
-// commands?.create({
-//     name: 'add',
-//     description: 'Adds two number.',
-//     options: [
-//         {
-//             name: 'num1',
-//             description: 'The first number',
-//             required: true,
-//             type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER
-//         },
-//         {
-//             name:  'num2',
-//             description: 'The second number',
-//             required: true,
-//             type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER
-//         }
-//     ]
-// })
-
-// Old way to use slashcommands
-
-// client.on('interactionCreate', async (interaction) =>  {
-//     if (!interaction.isCommand()) {
-//         return;
-//     }
-
-//     const { commandName, options } = interaction
-
-//     if (commandName === 'ping') {
-//         interaction.reply({
-//             content: 'pong',
-//             ephemeral: true,
-//         })
-//     } else if (commandName === 'add') {
-//         const num1 = options.getNumber('num1')!
-//         const num2 = options.getNumber('num2')!
-        
-//         await interaction.deferReply({
-//             ephemeral: true,
-//         })
-
-//         await new Promise(resolve => setTimeout(resolve, 5000))
-
-//         await interaction.editReply({
-//             content: `The sum is ${num1 + num2}`,
-//         })
-//     }
-// })
+client.login(process.env.TOKEN);
